4321eee6788573ce320d5cc5c6b5a6374351918d
Zhulanov Anton <zhulanoff2007@gmail.com>
2026-03-28 17:37:47 +0300

    6.2 Практикуемся в работе с HTTPX Практикуемся в работе с HTTPX: создание пользователя

---
+++ httpx_create_user.py
@@ -0,0 +1,15 @@
+import httpx
+
+from tools.fakers import get_random_email
+
+payload = {
+    "email": get_random_email(),
+    "password": "string",
+    "lastName": "string",
+    "firstName": "string",
+    "middleName": "string"
+}
+response = httpx.post("http://localhost:8000/api/v1/users", json=payload)
+
+print(response.status_code)
+print(response.json())
