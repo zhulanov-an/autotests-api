3b1759c0b76b67e6f21218afc9df1aa627c0b0e2
Zhulanov Anton <zhulanoff2007@gmail.com>
2026-03-31 20:04:26 +0300

    6.5 Практикуемся в использовании API клиентов Практикуемся в использовании API-клиентов

--- clients/courses/courses_client.py
+++ clients/courses/courses_client.py
@@ -3,7 +3,32 @@ from typing import TypedDict
 from httpx import Response
 
 from clients.api_client import APIClient
+from clients.files.files_client import File
 from clients.private_http_builder import AuthenticationUserDict, get_private_http_client
+from clients.users.private_users_client import User
+
+
+# Добавили описание структуры курса
+class Course(TypedDict):
+    """
+    Описание структуры курса.
+    """
+    id: str
+    title: str
+    maxScore: int
+    minScore: int
+    description: str
+    previewFile: File  # Вложенная структура файла
+    estimatedTime: str
+    createdByUser: User  # Вложенная структура пользователя
+
+
+# Добавили описание структуры ответа на создание курса
+class CreateCourseResponseDict(TypedDict):
+    """
+    Описание структуры ответа создания курса.
+    """
+    course: Course
 
 
 class GetCoursesQueryDict(TypedDict):
@@ -88,6 +113,11 @@ class CoursesClient(APIClient):
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.delete(f"/api/v1/courses/{course_id}")
+
+    # Добавили новый метод
+    def create_course(self, request: CreateCourseRequestDict) -> CreateCourseResponseDict:
+        response = self.create_course_api(request)
+        return response.json()
 
 
 # Добавляем builder для CoursesClient
