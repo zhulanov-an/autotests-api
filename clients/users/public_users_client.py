8656ad6c1d1a5c20812cd4b86b1205c74fad6316
Zhulanov Anton <zhulanoff2007@gmail.com>
2026-03-31 19:49:52 +0300

    6.5 Практикуемся в использовании API клиентов Практикуемся в использовании API-клиентов

--- clients/users/public_users_client.py
+++ clients/users/public_users_client.py
@@ -6,6 +6,26 @@ from clients.api_client import APIClient
 from clients.public_http_builder import get_public_http_client
 
 
+# Добавили описание структуры пользователя
+class User(TypedDict):
+    """
+    Описание структуры пользователя.
+    """
+    id: str
+    email: str
+    lastName: str
+    firstName: str
+    middleName: str
+
+
+# Добавили описание структуры ответа создания пользователя
+class CreateUserResponseDict(TypedDict):
+    """
+    Описание структуры ответа создания пользователя.
+    """
+    user: User
+
+
 class CreateUserRequestDict(TypedDict):
     """
     Описание структуры запроса на создание пользователя.
@@ -29,6 +49,11 @@ class PublicUsersClient(APIClient):
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.post("/api/v1/users", json=request)
+
+    # Добавили новый метод
+    def create_user(self, request: CreateUserRequestDict) -> CreateUserResponseDict:
+        response = self.create_user_api(request)
+        return response.json()
 
 
 # Добавляем builder для PublicUsersClient
