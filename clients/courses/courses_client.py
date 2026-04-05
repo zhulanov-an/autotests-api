631eaad041c6eb47b801d720e064f7df0e0ef65d
Anton Zhulanov <zhulanoff2007@gmail.com>
2026-04-05 18:21:31 +0300

    fix query 7.2 Применяем Pydantic в API клиентах Практикуемся в применении Pydantic в API клиентах

--- clients/courses/courses_client.py
+++ clients/courses/courses_client.py
@@ -18,7 +18,7 @@ class CoursesClient(APIClient):
         :param query: Pydantic-модель с userId.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.get("/api/v1/courses", params=query)
+        return self.get("/api/v1/courses", params=query.model_dump(by_alias=True))
 
     def get_course_api(self, course_id: str) -> Response:
         """
--- clients/exercises/exercises_client.py
+++ clients/exercises/exercises_client.py
@@ -19,7 +19,7 @@ class ExercisesClient(APIClient):
         :param query: Pydantic-модель с courseId.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
-        return self.get("/api/v1/exercises", params=query)
+        return self.get("/api/v1/exercises", params=query.model_dump(by_alias=True))
 
     def get_exercise_api(self, exercise_id: str) -> Response:
         """
