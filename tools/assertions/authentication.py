516469f7e97e1b1a42f34a111c1914a5140b81a9
Anton Zhulanov <zhulanoff2007@gmail.com>
2026-04-10 22:09:32 +0300

    8.2 Написание API автотестов используя Pytest Практика написания API автотестов

---
+++ tools/assertions/authentication.py
@@ -0,0 +1,14 @@
+from clients.authentication.authentication_schema import LoginResponseSchema
+from tools.assertions.base import assert_equal, assert_is_true
+
+
+def assert_login_response(response: LoginResponseSchema):
+    """
+    Проверяет корректность ответа при успешной авторизации.
+
+    :param response: Объект ответа с токенами авторизации.
+    :raises AssertionError: Если какое-либо из условий не выполняется.
+    """
+    assert_equal(response.token.token_type, "bearer", "token_type")
+    assert_is_true(response.token.access_token, "access_token")
+    assert_is_true(response.token.refresh_token, "refresh_token")
--- tools/assertions/base.py
+++ tools/assertions/base.py
@@ -30,3 +30,17 @@ def assert_equal(actual: Any, expected: Any, name: str):
         f'Expected value: {expected}. '
         f'Actual value: {actual}'
     )
+
+
+def assert_is_true(actual: Any, name: str):
+    """
+    Проверяет, что фактическое значение является истинным.
+
+    :param name: Название проверяемого значения.
+    :param actual: Фактическое значение.
+    :raises AssertionError: Если фактическое значение ложно.
+    """
+    assert actual, (
+        f'Incorrect value: "{name}". '
+        f'Expected true value but got: {actual}'
+    )
