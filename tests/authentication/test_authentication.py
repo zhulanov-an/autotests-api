49b9e974ffd6c0fd7510fb02ef563d59a3e0ec20
Anton Zhulanov <zhulanoff2007@gmail.com>
2026-04-18 17:45:12 +0300

    9.1 Рефакторинг и подготовка к написанию API автотестов Использование тестовых классов

--- tests/authentication/test_authentication.py
+++ tests/authentication/test_authentication.py
@@ -12,12 +12,13 @@ from tools.assertions.schema import validate_json_schema
 
 @pytest.mark.regression
 @pytest.mark.authentication
-def test_login(function_user: UserFixture, authentication_client: AuthenticationClient):
-    login_request = LoginRequestSchema(email=function_user.email, password=function_user.password)
-    login_response = authentication_client.login_api(login_request)
+class TestAuthentication:
+    def test_login(self, function_user: UserFixture, authentication_client: AuthenticationClient):
+        login_request = LoginRequestSchema(email=function_user.email, password=function_user.password)
+        login_response = authentication_client.login_api(login_request)
 
-    login_response_data = LoginResponseSchema.model_validate_json(login_response.text)
-    assert_status_code(login_response.status_code, HTTPStatus.OK)
-    assert_login_response(login_response_data)
+        login_response_data = LoginResponseSchema.model_validate_json(login_response.text)
+        assert_status_code(login_response.status_code, HTTPStatus.OK)
+        assert_login_response(login_response_data)
 
-    validate_json_schema(login_response.json(), login_response_data.model_json_schema())
+        validate_json_schema(login_response.json(), login_response_data.model_json_schema())
--- tests/users/test_users.py
+++ tests/users/test_users.py
@@ -12,42 +12,24 @@ from tools.assertions.users import assert_create_user_response, assert_get_user_
 from tools.fakers import fake
 
 
-@pytest.mark.users  # Добавили маркировку users
-@pytest.mark.regression  # Добавили маркировку regression
-@pytest.mark.parametrize(  # параметризация доменов электронной почты
-    "domain",
-    [
-        "mail.ru",
-        "gmail.com",
-        "example.com"
-    ]
-)
-def test_create_user(domain: str, public_users_client: PublicUsersClient):
-    # Формируем тело запроса на создание пользователя
-    request = CreateUserRequestSchema(email=fake.email(domain=domain))
-    # Отправляем запрос на создание пользователя
-    response = public_users_client.create_user_api(request)
+@pytest.mark.users
+@pytest.mark.regression
+class TestUsers:
 
-    # Инициализируем модель ответа на основе полученного JSON в ответе
-    # Также благодаря встроенной валидации в Pydantic дополнительно убеждаемся, что ответ корректный
-    response_data = CreateUserResponseSchema.model_validate_json(response.text)
+    @pytest.mark.parametrize("domain", ["mail.ru", "gmail.com", "example.com"])
+    def test_create_user(self, domain: str, public_users_client: PublicUsersClient):
+        request = CreateUserRequestSchema(email=fake.email(domain=domain))
+        response = public_users_client.create_user_api(request)
+        response_data = CreateUserResponseSchema.model_validate_json(response.text)
 
-    # Проверяем статус-код ответа
-    assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_create_user_response(request, response_data)
+        validate_json_schema(response.json(), response_data.model_json_schema())
 
-    # Проверяем, что данные ответа совпадают с данными запроса
-    assert_create_user_response(request, response_data)
+    def test_get_user_me(self, function_user: UserFixture, private_users_client: PrivateUsersClient):
+        get_me_response = private_users_client.get_user_me_api()
 
-    # Проверяем, что тело ответа соответствует ожидаемой JSON-схеме
-    validate_json_schema(response.json(), response_data.model_json_schema())
-
-
-@pytest.mark.users  # Добавили маркировку users
-@pytest.mark.regression  # Добавили маркировку regression
-def test_get_user_me(function_user: UserFixture, private_users_client: PrivateUsersClient):
-    get_me_response = private_users_client.get_user_me_api()
-
-    assert_status_code(get_me_response.status_code, HTTPStatus.OK)
-    get_me_response_data = GetUserResponseSchema.model_validate_json(get_me_response.text)
-    validate_json_schema(get_me_response.json(), get_me_response_data.model_json_schema())
-    assert_get_user_response(get_me_response_data, function_user.response)
+        assert_status_code(get_me_response.status_code, HTTPStatus.OK)
+        get_me_response_data = GetUserResponseSchema.model_validate_json(get_me_response.text)
+        validate_json_schema(get_me_response.json(), get_me_response_data.model_json_schema())
+        assert_get_user_response(get_me_response_data, function_user.response)
