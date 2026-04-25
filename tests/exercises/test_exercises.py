b304de2a34fc0372c108adc8bb5205621f2218e0
Zhulanov Anton <zhulanoff2007@gmail.com>
2026-04-25 20:57:55 +0300

    9.4 Практикуемся в написании API автотестов. Часть 3 Практика написания API автотестов для работы с заданиями. Получение задания

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -3,10 +3,12 @@ from http import HTTPStatus
 import pytest
 
 from clients.exercises.exercises_client import ExercisesClient
-from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema
+from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
+    GetExerciseResponseSchema
 from fixtures.courses import CourseFixture
+from fixtures.exercises import ExerciseFixture
 from tools.assertions.base import assert_status_code
-from tools.assertions.exercises import assert_create_exercise_response
+from tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response
 from tools.assertions.schema import validate_json_schema
 
 
@@ -23,3 +25,12 @@ class TestExercises:
                                         create_exercise_request=create_exercise_request)
 
         validate_json_schema(create_exercise_response.json(), create_exercise_response_data.model_json_schema())
+
+    def test_get_exercise(self, exercises_client: ExercisesClient, function_exercise: ExerciseFixture):
+        get_exercise_response = exercises_client.get_exercise_api(exercise_id=function_exercise.response.exercise.id)
+        get_exercise_response_data = GetExerciseResponseSchema.model_validate_json(get_exercise_response.text)
+
+        assert_status_code(get_exercise_response.status_code, HTTPStatus.OK)
+        assert_get_exercise_response(get_exercise_schema_response=get_exercise_response_data,
+                                     create_exercise_response=function_exercise.response)
+        validate_json_schema(get_exercise_response.json(), get_exercise_response_data.model_json_schema())
--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -1,4 +1,5 @@
-from clients.exercises.exercises_schema import CreateExerciseRequestSchema, ExerciseSchema
+from clients.exercises.exercises_schema import CreateExerciseRequestSchema, ExerciseSchema, GetExerciseResponseSchema, \
+    CreateExerciseResponseSchema
 from tools.assertions.base import assert_equal
 
 
@@ -25,3 +26,32 @@ def assert_create_exercise_response(create_exercise_schema_response: ExerciseSch
                  create_exercise_request.description, "description")
     assert_equal(create_exercise_schema_response.estimated_time,
                  create_exercise_request.estimated_time, "estimated_time")
+
+
+def assert_exercise(actual_exercise_schema: ExerciseSchema, expected_exercise_schema: ExerciseSchema):
+    """
+    Проверяет фактическую и ожидаемую модель упражнения
+    :param actual_exercise_schema: Актуальная модель упражнения
+    :param expected_exercise_schema: Ожидаемая модель упражнения.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    assert_equal(actual_exercise_schema.id, expected_exercise_schema.id, "id")
+    assert_equal(actual_exercise_schema.title, expected_exercise_schema.title, "title")
+    assert_equal(actual_exercise_schema.course_id, expected_exercise_schema.course_id, "course_id")
+    assert_equal(actual_exercise_schema.max_score, expected_exercise_schema.max_score, "max_score")
+    assert_equal(actual_exercise_schema.min_score, expected_exercise_schema.min_score, "min_score")
+    assert_equal(actual_exercise_schema.order_index, expected_exercise_schema.order_index, "order_index")
+    assert_equal(actual_exercise_schema.description, expected_exercise_schema.description, "description")
+    assert_equal(actual_exercise_schema.estimated_time, expected_exercise_schema.estimated_time, "estimated_time")
+
+
+def assert_get_exercise_response(get_exercise_schema_response: GetExerciseResponseSchema,
+                                 create_exercise_response: CreateExerciseResponseSchema):
+    """
+    Проверяет модель упражнения от запроса получения упражнения с моделью ответа на создание упражнения
+    :param get_exercise_schema_response: Ответ получения упражнения.
+    :param create_exercise_response: Ответ создания упражнения.
+    :raises AssertionError: Если хотя бы одно поле упражнения в ответах не совпадает.
+    """
+    assert_exercise(actual_exercise_schema=get_exercise_schema_response.exercise,
+                    expected_exercise_schema=create_exercise_response.exercise)
