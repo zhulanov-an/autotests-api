0324b3326deb63711da9b0f64f783b48be5958a9
Zhulanov Anton <zhulanoff2007@gmail.com>
2026-04-25 22:15:07 +0300

    9.4 Практикуемся в написании API автотестов. Часть 3 Практика написания API автотестов для работы с заданиями. Получение списка заданий

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -5,12 +5,13 @@ import pytest
 from clients.errors_schema import InternalErrorResponseSchema
 from clients.exercises.exercises_client import ExercisesClient
 from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
-    GetExerciseResponseSchema, UpdateExerciseRequestSchema, UpdateExerciseResponseSchema
+    GetExerciseResponseSchema, UpdateExerciseRequestSchema, UpdateExerciseResponseSchema, GetExercisesQuerySchema, \
+    GetExercisesResponseSchema
 from fixtures.courses import CourseFixture
 from fixtures.exercises import ExerciseFixture
 from tools.assertions.base import assert_status_code
 from tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response, \
-    assert_update_exercise_response, assert_exercise_not_found_response
+    assert_update_exercise_response, assert_exercise_not_found_response, assert_get_exercises_response
 from tools.assertions.schema import validate_json_schema
 
 
@@ -63,3 +64,16 @@ class TestExercises:
         assert_exercise_not_found_response(actual=get_exercise_response_data)
 
         validate_json_schema(get_exercise_response.json(), get_exercise_response_data.model_json_schema())
+
+    def test_get_exercises(self, exercises_client: ExercisesClient,
+                           function_course: CourseFixture,
+                           function_exercise: ExerciseFixture):
+        get_exercises_query = GetExercisesQuerySchema(course_id=function_course.response.course.id)
+        get_exercises_response = exercises_client.get_exercises_api(query=get_exercises_query)
+
+        assert_status_code(get_exercises_response.status_code, HTTPStatus.OK)
+
+        get_exercises_response_data = GetExercisesResponseSchema.model_validate_json(get_exercises_response.text)
+        assert_get_exercises_response(get_exercises_response=get_exercises_response_data,
+                                      create_exercise_responses=[function_exercise.response])
+        validate_json_schema(get_exercises_response.json(), get_exercises_response_data.model_json_schema())
--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -1,7 +1,7 @@
 from clients.errors_schema import InternalErrorResponseSchema
 from clients.exercises.exercises_schema import CreateExerciseRequestSchema, ExerciseSchema, GetExerciseResponseSchema, \
-    CreateExerciseResponseSchema, UpdateExerciseRequestSchema
-from tools.assertions.base import assert_equal
+    CreateExerciseResponseSchema, UpdateExerciseRequestSchema, GetExercisesResponseSchema
+from tools.assertions.base import assert_equal, assert_length
 from tools.assertions.errors import assert_internal_error_response
 
 
@@ -85,3 +85,17 @@ def assert_exercise_not_found_response(actual: InternalErrorResponseSchema):
     """
     expected = InternalErrorResponseSchema(details="Exercise not found")
     assert_internal_error_response(actual, expected)
+
+
+def assert_get_exercises_response(get_exercises_response: GetExercisesResponseSchema,
+                                  create_exercise_responses: list[CreateExerciseResponseSchema]):
+    """
+    Проверяет, что ответ на получение списка заданий соответствует ответам на их создание.
+    :param get_exercises_response: Ответ API при запросе списка заданий.
+    :param create_exercise_responses: Список API ответов при создании заданий.
+    :raises AssertionError: Если данные заданий не совпадают.
+    """
+    assert_length(get_exercises_response.exercises, create_exercise_responses, "exercises")
+
+    for index, create_exercise_response in enumerate(create_exercise_responses):
+        assert_exercise(get_exercises_response.exercises[index], create_exercise_response.exercise)
