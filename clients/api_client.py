e6bd2eab7f7f4a1894c1bd023e1f4991227666cd
Anton Zhulanov <zhulanoff2007@gmail.com>
2026-05-02 15:29:35 +0300

    10.3 Добавление Allure шагов в автотесты Добавление allure.step в API-клиенты

--- clients/api_client.py
+++ clients/api_client.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+import allure
 from httpx import Client, URL, QueryParams, Response
 from httpx._types import RequestData, RequestFiles
 
@@ -13,6 +14,7 @@ class APIClient:
         """
         self.client = client
 
+    @allure.step("Make GET request to {url}")
     def get(self, url: URL | str, params: QueryParams | None = None) -> Response:
         """
         Выполняет GET-запрос.
@@ -23,6 +25,7 @@ class APIClient:
         """
         return self.client.get(url, params=params)
 
+    @allure.step("Make POST request to {url}")
     def post(
             self,
             url: URL | str,
@@ -41,6 +44,7 @@ class APIClient:
         """
         return self.client.post(url, json=json, data=data, files=files)
 
+    @allure.step("Make PATCH request to {url}")
     def patch(self, url: URL | str, json: Any | None = None) -> Response:
         """
         Выполняет PATCH-запрос (частичное обновление данных).
@@ -51,6 +55,7 @@ class APIClient:
         """
         return self.client.patch(url, json=json)
 
+    @allure.step("Make DELETE request to {url}")
     def delete(self, url: URL | str) -> Response:
         """
         Выполняет DELETE-запрос (удаление данных).
